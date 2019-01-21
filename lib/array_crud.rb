52cdaba870e68b15ae9e498d17741c962ba7cd6b
Moses Olawoyin <m.bastford@gmail.com>
2019-01-21 22:14:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 def add_element_to_end_of_array(array, element)
   
   array = array.push [1]
-  array_of_numbers = array_of_numbers.push[5]
+  
 end
 
 def add_element_to_start_of_array(array, element)
