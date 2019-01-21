edb13a0c716c1c39eeaa3a7d96e5bd4d8393afd4
Moses Olawoyin <m.bastford@gmail.com>
2019-01-21 22:47:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,13 +28,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array_numbers = [1,2,3,4,5]
-  array_numbers = array_numbers[]
+  
 end
 
 def retrieve_first_element_from_array(array)
-  array_numbers = [1,2,3,4,5]
-  array_numbers = array_numbers[0]
+  array = array[0]
 end
 
 def retrieve_last_element_from_array(array)
