dab43f84e1ee9cda331ec14432f35e18d42553d5
Moses Olawoyin <m.bastford@gmail.com>
2019-01-21 22:52:12 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
+  empty_array = []
   
 end
 
 def create_an_array
-  
+  list_books = ["calculus" , "Integration" , "probability","owe"]
 end
 
 def add_element_to_end_of_array(array, element)
   
+  array = array.push "arrays!"
+
 end
 
 def add_element_to_start_of_array(array, element)
+ array = array.unshift "wow"
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+ array = array.pop
 end
 
 def remove_element_from_start_of_array(array)
+  array = array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  index_number = "am"
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = array[-1]
 end
