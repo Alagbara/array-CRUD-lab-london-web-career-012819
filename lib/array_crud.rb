9cfcd451cba6b7a164ef12b6b93ae775c6364079
Moses Olawoyin <m.bastford@gmail.com>
2019-01-21 21:50:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array_numbers = [1,2,3,4,5]
+  array_number = array_number.pop[5]
   
 end
 
