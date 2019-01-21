71f47e2ea90f86e4e5b281469395c9e35c3bcd58
Moses Olawoyin <m.bastford@gmail.com>
2019-01-21 22:27:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
- array = array.shift "wow"
+ add_element_to_start_of_array(array, element).shift
   
 end
 
